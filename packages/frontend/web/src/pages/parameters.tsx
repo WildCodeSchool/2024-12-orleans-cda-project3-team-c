2a4aa8d7feb11fd473456ea51eb7fd809ba44499
alesbesdria <melissa@meirsman.fr>
2025-03-30 13:14:33 +0200

    first commit new branch parameters

--- packages/frontend/web/src/pages/parameters.tsx
+++ packages/frontend/web/src/pages/parameters.tsx
@@ -1,5 +1,17 @@
-// import NavBar from '@/components/navbar-left-web';
+import arrowLeftIcon from '../assets/icons/arrow-left-white.svg';
 
 export default function Parameters() {
-  return <>{'salut'}</>;
+  return (
+    <section className='flex flex-col'>
+      <div className='flex'>
+        <img
+          className='size-[32px]'
+          src={arrowLeftIcon}
+          alt='arrow left icon'
+        />
+        <h2 className='text-[24px]'>{'Parameters'}</h2>
+      </div>
+      <section className='flex flex-col' />
+    </section>
+  );
 }
