4447c903ea9368470a4620d752da8d289ccf632a
alesbesdria <melissa@meirsman.fr>
2025-04-01 11:44:31 +0200

    fix too much css and section and suppr 2 files navbarfor just 1

--- packages/frontend/web/src/app.tsx
+++ packages/frontend/web/src/app.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
 import NavBarLeftWeb from './components/navbar-left-web';
-import NavBarMobile from './components/navbar-mobile';
 
 export default function App() {
   return (
     <div className='flex h-dvh w-full'>
       <NavBarLeftWeb />
-      <NavBarMobile />
       <main>
         <Outlet />
       </main>
