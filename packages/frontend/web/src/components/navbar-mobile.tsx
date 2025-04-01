df8abd06aaa3c8a455b82f34f6c3a79e9c51a9bd
alesbesdria <melissa@meirsman.fr>
2025-04-01 09:50:30 +0200

    fix reviews and aria-label

--- packages/frontend/web/src/components/navbar-mobile.tsx
+++ packages/frontend/web/src/components/navbar-mobile.tsx
@@ -8,36 +8,56 @@ import userIcon from '../assets/icons/user-white.svg';
 
 export default function NavBarMobile() {
   return (
-    <section className='h-dvh w-dvw sm:hidden'>
-      <nav className='fixed bottom-0 mb-4 h-[50px] w-dvw pt-2'>
+    <div className='h-dvh w-dvw sm:hidden'>
+      <nav className='fixed bottom-0 h-[50px] w-dvw pt-2 pb-4'>
         <ul className='relative flex w-dvw justify-around pt-4 after:absolute after:top-0 after:left-[10%] after:w-[80%] after:border-t'>
           <li>
-            <Link to='/'>
-              <img className='size-8' src={homeIcon} alt='Home' />
+            <Link to='/' aria-label='Accueil'>
+              <img
+                className='size-8'
+                src={homeIcon}
+                alt=''
+                aria-hidden='true'
+              />
             </Link>
           </li>
           <li>
-            <Link to='/search'>
-              <img className='size-8' src={searchIcon} alt='Search' />
+            <Link to='/search' aria-label='Recherche'>
+              <img
+                className='size-8'
+                src={searchIcon}
+                alt=''
+                aria-hidden='true'
+              />
             </Link>
           </li>
           <li>
-            <Link to='/add'>
-              <img className='size-8' src={addIcon} alt='Add a post' />
+            <Link to='/add' aria-label='Ajouter un post'>
+              <img className='size-8' src={addIcon} alt='' aria-hidden='true' />
             </Link>
           </li>
           <li>
-            <Link to='/chat'>
-              <img className='size-8' src={chatIcon} alt='Chat' />
+            <Link to='/chat' aria-label='Messages'>
+              <img
+                className='size-8'
+                src={chatIcon}
+                alt=''
+                aria-hidden='true'
+              />
             </Link>
           </li>
           <li>
-            <Link to='/profile'>
-              <img className='size-8' src={userIcon} alt='Profile' />
+            <Link to='/profile' aria-label='Profil'>
+              <img
+                className='size-8'
+                src={userIcon}
+                alt=''
+                aria-hidden='true'
+              />
             </Link>
           </li>
         </ul>
       </nav>
-    </section>
+    </div>
   );
 }
