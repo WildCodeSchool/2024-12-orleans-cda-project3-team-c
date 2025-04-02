cc38f88d09bbc46c1220a96428ea79fb6c4c852d
Luc Martial <lucmartial@msn.com>
2025-04-02 16:32:02 +0200

    feature/User-suggestion component fix

--- packages/frontend/web/src/router.tsx
+++ packages/frontend/web/src/router.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import UserSuggestion from './components/user-suggestion';
 import Home from './pages/home';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  },
-  {
-    path: '/s',
-    element: <UserSuggestion />,
   },
 ]);
 
