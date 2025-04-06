4e66452883279d3b6623579f5ed8920e4d46b3bf
alesbesdria <melissa@meirsman.fr>
2025-04-06 22:08:42 +0200

    continue  responsive

--- packages/frontend/web/src/app.tsx
+++ packages/frontend/web/src/app.tsx
@@ -4,9 +4,9 @@ import NavBar from './components/navbar';
 
 export default function App() {
   return (
-    <div className='flex h-dvh w-full'>
+    <div className='flex h-full w-full'>
       <NavBar />
-      <main className='w-full'>
+      <main className='h-full w-full'>
         <Outlet />
       </main>
     </div>
--- packages/frontend/web/src/pages/parameters.tsx
+++ packages/frontend/web/src/pages/parameters.tsx
@@ -7,50 +7,58 @@ import userIcon from '../assets/icons/user-white.svg';
 
 export default function Parameters() {
   return (
-    <section className='mt-40 flex flex-col items-center'>
+    <section className='item-start mt-4 flex h-full flex-col pl-4 sm:mt-40 sm:items-center sm:pl-0'>
       <div className='flex items-center'>
         <Link to='/profile'>
           <img className='h-8 w-8' src={arrowLeftIcon} alt='arrow left icon' />
         </Link>
         <h2 className='font-title ml-16 text-xl'>{'Parameters'}</h2>
       </div>
-      <ul className='mt-4 flex h-full flex-col'>
-        <li className='h-12 max-w-fit'>
-          <Link
-            className='flex flex-row items-center text-center'
-            to='/profile-informations'
-          >
-            <img
-              className='mt-2 mr-4 mb-2 h-8 w-8'
-              src={userIcon}
-              alt='profile informations icon'
-            />
-            {'Profile Informations'}
-          </Link>
-        </li>
-        <li className='h-12 max-w-fit'>
-          <Link
-            className='flex flex-row items-center text-center'
-            to='/account-settings'
-          >
-            <img
-              className='mt-2 mr-4 mb-2 h-8 w-8'
-              src={gearIcon}
-              alt='account settings icon'
-            />
-            {'Account Settings'}
-          </Link>
-        </li>
-        <li className='h-12 max-w-fit'>
-          <Link className='flex flex-row items-center text-center' to='/logout'>
-            <img
-              className='mt-2 mr-4 mb-2 h-8 w-8'
-              src={logoutIcon}
-              alt='logout icon'
-            />
-            {'Log Out'}
-          </Link>
-        </li>
+      <ul className='mt-4 flex h-full flex-col justify-between pl-4 sm:pl-0'>
+        <div>
+          <li className='h-12 max-w-fit'>
+            <Link
+              className='flex flex-row items-center text-center'
+              to='/profile-informations'
+            >
+              <img
+                className='mt-2 mr-4 mb-2 h-8 w-8'
+                src={userIcon}
+                alt='profile informations icon'
+              />
+              {'Profile Informations'}
+            </Link>
+          </li>
+          <li className='h-12 max-w-fit'>
+            <Link
+              className='flex flex-row items-center text-center'
+              to='/account-settings'
+            >
+              <img
+                className='mt-2 mr-4 mb-2 h-8 w-8'
+                src={gearIcon}
+                alt='account settings icon'
+              />
+              {'Account Settings'}
+            </Link>
+          </li>
+        </div>
+
+        <div>
+          <li className='h-12 max-w-fit'>
+            <Link
+              className='flex flex-row items-center text-center'
+              to='/logout'
+            >
+              <img
+                className='mt-2 mr-4 mb-2 h-8 w-8'
+                src={logoutIcon}
+                alt='logout icon'
+              />
+              {'Log Out'}
+            </Link>
+          </li>
+        </div>
       </ul>
     </section>
   );
