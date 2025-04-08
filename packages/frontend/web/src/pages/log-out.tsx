6c7481b79703ef14cab91e479e60e38fc1bc530c
alesbesdria <melissa@meirsman.fr>
2025-04-08 10:07:52 +0200

    add comms for functions

--- packages/frontend/web/src/pages/log-out.tsx
+++ packages/frontend/web/src/pages/log-out.tsx
@@ -1,7 +1,5 @@
 export default function LogOut() {
   return;
-  {
-    /* voir avec alhassane pour la co et deco */
-  }
   // pas une page mais une modale de validation de deconnexion à faire plus tard
 }
+// voir avec alhassane pour la co et deco
