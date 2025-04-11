4ebb3a4cd67f607f43d6bc69c8c0e218eabee69b
alesbesdria <melissa@meirsman.fr>
2025-04-11 11:33:08 +0200

    it worksssssss

--- packages/frontend/web/src/components/profile-own.tsx
+++ packages/frontend/web/src/components/profile-own.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import userApiConnection, {
   type UserPost,
   type UserProfile,
 } from '../api-connection/user-api-connection';
+import menu from '../assets/icons/menu-square-white.svg';
 
 export default function ProfileOwn() {
   const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
@@ -46,29 +48,35 @@ export default function ProfileOwn() {
                   {'121'}
                 </p>
               </div>
+              <Link to='/parameters'>
+                <img className='size-6 md:size-8' src={menu} alt='Menu' />
+              </Link>
             </div>
 
             <ul className='mb-2 flex gap-4 text-xs sm:text-base'>
-              <li className='flex items-center gap-1'>
-                <span className='text-turquoise-blue-400'>{'2'}</span>
-                <span>{'posts'}</span>
-              </li>
-              <li className='flex items-center gap-1'>
-                <span className='text-turquoise-blue-400'>{'5'}</span>
-                <span>{'followers'}</span>
-              </li>
-              <li className='flex items-center gap-1'>
-                <span className='text-turquoise-blue-400'>{'18'}</span>
-                <span>{'following'}</span>
-              </li>
+              {[
+                { label: 'posts', count: 2 },
+                { label: 'followers', count: 5 },
+                { label: 'following', count: 18 },
+              ].map(({ label, count }) => (
+                <li key={label} className='flex items-center gap-1'>
+                  <span className='text-turquoise-blue-400'>{count}</span>
+                  <span>{label}</span>
+                </li>
+              ))}
             </ul>
-
+            {/* Selectionner la bio de l'utilisateur actuel */}
             <p className='hidden max-w-lg text-base md:block'>
               {userProfile.biography}
             </p>
           </div>
         </div>
       </div>
+
+      {/* Selectionner la bio de l'utilisateur actuel pour mobile */}
+      <p className='border-turquoise-blue-400 max-w-lg border-b-2 pb-4 text-sm md:hidden'>
+        {userProfile.biography}
+      </p>
 
       <section className='flex flex-wrap justify-center gap-2 pt-2'>
         {posts.map((post, index) => (
