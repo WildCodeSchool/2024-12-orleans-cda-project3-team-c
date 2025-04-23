2cce06709d519c51313d01e3da9af53ad6ae526f
alesbesdria <melissa@meirsman.fr>
2025-04-23 10:27:45 +0200

    fix reviews

--- packages/backend/api/src/router.ts
+++ packages/backend/api/src/router.ts
@@ -1,7 +1,6 @@
 // src/router.ts
 import express from 'express';
 
-import followsRouter from './subrouters/follows-router';
 import postsRouter from './subrouters/posts-router';
 import tagsRouter from './subrouters/tags-router';
 import usersRouter from './subrouters/users-router';
@@ -11,7 +10,6 @@ const router = express.Router();
 router.use('/posts', postsRouter);
 router.use('/tags', tagsRouter);
 router.use('/users', usersRouter);
-router.use('/follows', followsRouter);
 
 router.use('*', (req, res) => {
   res.status(404).send(`Resource ${req.path} not found`);
