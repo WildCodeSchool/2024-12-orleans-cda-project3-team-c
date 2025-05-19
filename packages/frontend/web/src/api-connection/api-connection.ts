b8593d43cd0487a3b70670fb70df6921a69deeff
Teddy Aboim <teddy.aboim@gmail.com>
2025-05-19 16:31:41 +0200

    CD test 1

--- packages/frontend/web/src/api-connection/api-connection.ts
+++ packages/frontend/web/src/api-connection/api-connection.ts
@@ -1,10 +1,9 @@
 export default class ApiConnection {
-  apiUrl = import.meta.env.VITE_API_URL;
   ressource: string;
   ressourceUrl: string;
 
   constructor(ressource: string) {
     this.ressource = ressource;
-    this.ressourceUrl = `${this.apiUrl}/${this.ressource}`;
+    this.ressourceUrl = `api/${this.ressource}`;
   }
 }
